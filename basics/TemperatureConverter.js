43156fa6a22e56f562cf30b8524263f5770642a3
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-12-19 22:01:53 -0600

    completed fahrenheit to celsius converter

---
+++ basics/TemperatureConverter.js
@@ -0,0 +1,16 @@
+let fahrenheit = 32;
+
+// calculate celsius and store in celsius variable
+// print that value
+
+// calculate kelvin value and store in variable
+// print that value
+
+function fToC(F) {
+	let C;
+	C = ((F - 32) * (5 / 9))
+	console.log(C.toFixed(1));
+}
+
+fToC(80);
+
